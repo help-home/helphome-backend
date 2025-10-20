3ef30f0f4adb3490ce120b2557d0ec124dcfe5db
Rowoon <dev.rowoon@gmail.com>
2025-10-20 14:21:14 +0900

    init

---
+++ src/main/java/com/example/helphomebackend/HelpHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.helphomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpHomeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helphomebackend/HelpHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helphomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpHomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
