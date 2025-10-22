78d0d4301d23af1bbd3fe9f6da640c7dc3da7d9f
Rowoon <dev.rowoon@gmail.com>
2025-10-22 15:18:44 +0900

    구분 컬럼 추가로 인한 규격 변경

--- src/main/java/com/example/helphomebackend/entity/Language.java
+++ src/main/java/com/example/helphomebackend/entity/Language.java
@@ -15,6 +15,9 @@ public class Language {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String category;
+
     @Column(nullable = false, length = 50)
     private String koName;
 
